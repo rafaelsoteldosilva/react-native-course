0c4d18282c65f0f1fb79f2cb9d3eabe622117604
Rafael <rafael.soteldo@gmail.com>
2025-07-05 12:27:46 -0400

    commiting

--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -1,6 +1,7 @@
+// app/login.tsx
 import {Text, View} from "react-native";
-// app/index.tsx
-export default function Index() {
+
+export default function LoginScreen() {
     return (
         <View
             style={{
@@ -9,7 +10,7 @@ export default function Index() {
                 alignItems: "center",
             }}
         >
-            <Text>This is the main index.tsx</Text>
+            <Text>This is the login screen</Text>
         </View>
     );
 }
