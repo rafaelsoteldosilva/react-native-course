4a79ede5811573fd4a563c248856ffc46b78080f
Rafael <rafael.soteldo@gmail.com>
2025-07-05 14:31:19 -0400

    commiting

---
+++ app/auth.tsx
@@ -0,0 +1,15 @@
+// app/auth.tsx
+
+import {KeyboardAvoidingView, Platform, Text, View} from "react-native";
+
+export default function AuthScreen() {
+    return (
+        <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+            <View>
+                <Text>Create Account</Text>
+            </View>
+        </KeyboardAvoidingView>
+    );
+}
